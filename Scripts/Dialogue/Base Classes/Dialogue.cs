e7e4fb4bfff7ef57e37e9acf51de8b3910a64dd5
lcfr822 <lcfr822@gmail.com>
2019-06-13 12:56:21 -0700

    Tweaks to Quest system and Reorganization ahead of Dialogue development.

---
+++ Scripts/Dialogue/Base Classes/Dialogue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Questify.Interaction.Dialogue
+{
+    public class Dialogue
+    {
+        public string dialogueText;
+
+        public Dialogue(string newDialogueText)
+        {
+            dialogueText = newDialogueText;
+        }
+    }
+}
